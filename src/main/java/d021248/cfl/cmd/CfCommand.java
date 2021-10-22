356e56eca9eea98d8044fbba3d61c3f4c743edfa
Matthias Steinwagner <matthias.steinwagner@sap.com>
2021-10-22 23:34:30 +0200

    refactor

--- src/main/java/d021248/cfl/cmd/CfCommand.java
+++ src/main/java/d021248/cfl/cmd/CfCommand.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.function.Consumer;
 
 class CfCommand extends Command {
@@ -25,11 +28,48 @@ class CfCommand extends Command {
 		return -1;
 	}
 
-	public static CfCommand apps() {
+	public static List<Application> apps() {
 		var command = new CfCommand("cf", "apps");
-		command.sync().in(CfCommand::toLogger).err(CfCommand::toLogger);
+		var appList = new ArrayList<Application>();
+		Consumer<InputStream> toApplicationList = is -> getApplicationList(is, appList);
+
+		command.sync().in(toApplicationList).err(CfCommand::toLogger);
 		command.start();
-		return command;
+		return appList;
+	}
+
+	private static List<Application> getApplicationList(InputStream is, ArrayList<Application> appList) {
+		final String APPS_FOUND_INDICATOR = "^name.*requested.*state.*instances.*memory.*disk.*urls.*";
+		String line;
+		var isApp = false;
+		try (var br = new BufferedReader(new InputStreamReader(is))) {
+			while ((line = br.readLine()) != null) {
+				CfCommand.logger.accept(line);
+				if (isApp) {
+					var app = new Application();
+					var a = line.split("\\s+");
+					app.setName(saveGet(a, 0));
+					app.setState(saveGet(a, 1));
+					app.setInstances(saveGet(a, 2));
+					app.setMemory(saveGet(a, 3));
+					app.setDisk(saveGet(a, 4));
+					app.setUrls(saveGet(a, 5));
+					appList.add(app);
+				}
+				if (line.matches(APPS_FOUND_INDICATOR)) {
+					isApp = true;
+				}
+			}
+		} catch (IOException e) {
+
+			CfCommand.logger.accept("Error: " + e.getMessage());
+		}
+		Collections.sort(appList, (l, r) -> l.getName().compareTo(r.getName()));
+		return appList;
+	}
+
+	private static String saveGet(String[] a, int i) {
+		return (a == null || i >= a.length || i < 0) ? "" : a[i];
 	}
 
 	public static CfCommand logs(String app) {
@@ -40,12 +80,12 @@ class CfCommand extends Command {
 	}
 
 	private static void toLogger(InputStream is) {
-		String prefix = ">";
+
 		String line;
 		try (var br = new BufferedReader(new InputStreamReader(is))) {
 			while ((line = br.readLine()) != null) {
 				if (!line.isEmpty()) {
-					logger.accept(String.format("%s %s", prefix, line.trim()));
+					logger.accept(line.trim());
 				}
 			}
 		} catch (IOException e) {
--- src/main/java/d021248/cfl/cmd/TestCfCommand.java
+++ src/main/java/d021248/cfl/cmd/TestCfCommand.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class TestCfCommand {
     public static void main(String[] args) throws InterruptedException, IOException {
         // CfCommand.logs("mkv-srv");
-        CfCommand.apps();
+        var list = CfCommand.apps();
+        System.out.println(list);
         // Thread.sleep(60_000);
         // Command.stopAll();
     }
